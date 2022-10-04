c0f45a0aa3a6f332051ba7a51978cbf53ae23689
KarvaMies <80526519+KarvaMies@users.noreply.github.com>
2022-10-04 11:46:22 +0300

    Task 1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "./styles.css";
 
+/*
 document.getElementById("app").innerHTML = `
 <h1>Hello Vanilla!</h1>
 <div>
@@ -8,3 +9,32 @@ document.getElementById("app").innerHTML = `
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
 </div>
 `;
+*/
+
+const fetchData = async () => {
+  const url =
+    "https://geo.stat.fi/geoserver/wfs?service=WFS&version=2.0.0&request=GetFeature&typeName=tilastointialueet:kunta4500k&outputFormat=json&srsName=EPSG:4326";
+  const res = await fetch(url);
+  const data = await res.json();
+
+  initMap(data);
+};
+
+const initMap = (data) => {
+  let map = L.map("map", {
+    minZoom: -3
+  });
+
+  let geoJson = L.geoJSON(data, {
+    weight: 2
+  }).addTo(map);
+
+  let osm = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution: "© OpenStreetMap"
+  }).addTo(map);
+
+  map.fitBounds(geoJson.getBounds());
+};
+
+fetchData();
